cf4c45d0f3ac5e06fc2968e86f65dbac177918ac
VindiceCode <103221715+VindiceCode@users.noreply.github.com>
2024-04-08 18:23:46 -0600

    added user dictionary for assigned_to with associations.

--- callhandling/hawkvision/__init__.py
+++ callhandling/hawkvision/__init__.py
@@ -32,6 +32,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         updated_at = prospect.get('updated_at')
         prospect_id = prospect.get('id')
 
+        # Initialize a dictionary to store the associations between IDs and names
+        id_to_name = {}
+
+        # Assume you have a list of tuples, where each tuple contains a unique ID and a name
+        associations = [("6387", "Joe Prepolec"), ("6416", "Chaz Wenzel"), ("21999", "Chaz Wenzel"), ("22000", "Chaz Wenzel"), ("22001", "Chaz Wenzel"), ("22002", "Chaz Wenzel"), ("22003", "Chaz Wenzel"), ("22009", "Chaz Wenzel"), ("22004", "Chaz Wenzel"), ("14138", "Corie Walker"), ("21799", "Corie Walker"), ("22011", "Corie Walker"), ("22012", "Corie Walker"), ("22013", "Corie Walker"), ("22014", "Corie Walker"), ("22015", "Corie Walker"), ("22016", "Corie Walker"), ("22017", "Corie Walker"), ("11126", "Dani Hardy"), ("21798", "Dani Hardy"), ("22025", "Dani Hardy"), ("22026", "Dani Hardy"), ("22027", "Dani Hardy"), ("22028", "Dani Hardy"), ("22029", "Dani Hardy"), ("22030", "Dani Hardy"), ("22031", "Dani Hardy"), ("16469", "Eric Rayner"), ("21800", "Eric Rayner"), ("22034", "Eric Rayner"), ("22035", "Eric Rayner"), ("22037", "Eric Rayner"), ("22038", "Eric Rayner"), ("22039", "Eric Rayner"), ("22040", "Eric Rayner"), ("22041", "Eric Rayner"), ("18220", "Evan Walker"), ("21801", "Evan Walker"), ("22053", "Evan Walker"), ("22054", "Evan Walker"), ("22055", "Evan Walker"), ("22056", "Evan Walker"), ("22057", "Evan Walker"), ("22058", "Evan Walker"), ("22059", "Evan Walker"), ("18221", "Ian Evans"), ("21802", "Ian Evans"), ("22045", "Ian Evans"), ("22046", "Ian Evans"), ("22047", "Ian Evans"), ("22048", "Ian Evans"), ("22049", "Ian Evans"), ("22050", "Ian Evans"), ("22051", "Ian Evans"), ("7395", "Ian Melchor"), ("21795", "Ian Melchor"), ("22063", "Ian Melchor"), ("22064", "Ian Melchor"), ("22065", "Ian Melchor"), ("22066", "Ian Melchor"), ("22067", "Ian Melchor"), ("22068", "Ian Melchor"), ("22069", "Ian Melchor"), ("11125", "Kemuel Veloz"), ("21797", "Kemuel Veloz"), ("22074", "Kemuel Veloz"), ("22075", "Kemuel Veloz"), ("22076", "Kemuel Veloz"), ("22077", "Kemuel Veloz"), ("22078", "Kemuel Veloz"), ("22079", "Kemuel Veloz"), ("6413", "Melanie Trinh"), ("9934", "Sean Muscaro")]
+        # Populate the dictionary with the associations
+        for id, name in associations:
+            id_to_name[id] = name
+
         #Access the Additional Top-End Object and Message Objects
         additional = payload.get('additional')
         message = additional.get('message')
@@ -81,6 +90,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         if existing_contacts is None or len(existing_contacts.results) == 0:
         # Create a contact
             with rate_limiter:
+                # Assume assigned_to is a unique ID
+                assigned_to_name = id_to_name[assigned_to]
                 simple_public_object_input_for_create = SimplePublicObjectInputForCreate(
                     properties={
                         "firstname": first_name,
@@ -90,7 +101,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                         "city": city,
                         "state": state,
                         "zip": zip_code,
-                        "bonzo_owner": assigned_to,
+                        "bonzo_owner": assigned_to_name,
                         "bonzo_create_date": created_at,
                         "time_of_bonzo_response": event_date,
                         "bonzo_propsect_id": prospect_id,
